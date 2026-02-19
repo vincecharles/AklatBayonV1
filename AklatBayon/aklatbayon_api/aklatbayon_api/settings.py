f21694dc119d5f1e8a9176eafbc6cc83c5d5ac4e
vincecharles <vincecharles71@gmail.com>
2026-02-20 03:33:41 +0800

    feat: Add initial Django project settings including DRF, SimpleJWT, and CORS configurations.

--- AklatBayon/aklatbayon_api/aklatbayon_api/settings.py
+++ AklatBayon/aklatbayon_api/aklatbayon_api/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1', '.vercel.app').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,.vercel.app').split(',')
 
 
 # Application definition
